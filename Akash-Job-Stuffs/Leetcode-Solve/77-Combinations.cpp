154b01eb8a054663e30a0a635127c775220c003d
AkashPodder <anantaakash.podder@gmail.com>
2025-01-19 12:16:00 +0600

    - added "Solve-2" for Leetcode-77

--- Akash-Job-Stuffs/Leetcode-Solve/77-Combinations.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/77-Combinations.cpp
@@ -1,3 +1,4 @@
+// SOLVE-1
 class Solution {
 public:
     void solve(vector<int>& nums, vector<vector<int>> &ans, int k, int index, vector<int> &temp_v) {
@@ -31,5 +32,46 @@ public:
         solve(nums, ans, k, 0, temp_v);
 
         return ans;      
+    }
+};
+
+
+
+// SOLVE-2
+class Solution {
+public:
+    void solve(vector<int> &nums, int k, int index, vector<vector<int>> &ans, vector<int> &v){
+        if(index==nums.size()){
+            if(k==0){
+                ans.push_back(v);
+            }
+            return;
+        }
+
+        // Take
+        if(k>0){
+            v.push_back(nums[index]);
+            solve(nums, k-1, index+1, ans, v); // we Take the current element & move to the Next Index & REDUCE "k" with "-1"
+            v.pop_back();
+        }
+
+        // NOT Take (we Always Not Take)
+        solve(nums, k, index+1, ans, v); // we Only Move to the Next Index
+
+        return;
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        vector<vector<int>> ans;
+        
+        vector<int> nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+
+        solve(nums, k, 0, ans, v);
+
+        return ans;
     }
 };
