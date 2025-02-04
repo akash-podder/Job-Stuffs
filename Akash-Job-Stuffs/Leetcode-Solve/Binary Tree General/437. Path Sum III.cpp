e3896e0a47d8f2c7dd83c0c73aa2b00bd2022647
Akash Podder <anantaakash.podder@gmail.com>
2025-02-04 12:20:33 +0600

    - new solution added for "437. Path Sum III"

--- Akash-Job-Stuffs/Leetcode-Solve/Binary Tree General/437. Path Sum III.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/Binary Tree General/437. Path Sum III.cpp
@@ -1,3 +1,4 @@
+// Solve-1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +10,85 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    int solve(TreeNode* current, long long int targetSum){
+        // Leaf Node
+        if(current->left==NULL && current->right==NULL){
+            // we found 1 path so we return 1
+            if(targetSum-current->val==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+
+        int no_of_path=0;
+        
+        // as "targetSum=0", upto this Node it has 1 Path, so start with no_path_sum=1
+        if(targetSum-current->val==0){
+            no_of_path=1;
+        }
+
+        if(current->left!=NULL){
+            no_of_path+=solve(current->left, targetSum-current->val);
+        }
+
+        if(current->right!=NULL){
+            no_of_path+=solve(current->right, targetSum-current->val);
+        }
+
+        return no_of_path;
+    }
+
+    int solve2(TreeNode* current, long long int targetSum) {
+        if (current == NULL) {
+            return 0;
+        }
+
+        int no_of_path = 0;
+
+        // as "targetSum=0", upto this Node it has 1 Path, so start with no_path_sum=1
+        if (targetSum == current->val) {
+            no_of_path = 1;
+        }
+
+    
+        no_of_path+=solve2(current->left, targetSum-current->val);
+        
+        no_of_path+=solve2(current->right, targetSum-current->val);
+
+        
+        return no_of_path;
+    }
+
+    // we Traverse all Node & Treat that Node as the "ROOT" & then, Invoke "solve" Function to get Paths from that Node
+    void dfs(TreeNode* current, int targetSum, int &ans) {
+        if(current==NULL){
+            return;
+        }
+
+    // we consider a PARTICULAR Node as a Root Node & we Find Number of Paths where START will be that PARTICULAR NODE
+        ans+=solve2(current, targetSum);
+
+        dfs(current->left, targetSum, ans);
+        dfs(current->right, targetSum, ans);
+
+        return;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        int ans=0;
+
+        dfs(root, targetSum, ans);
+
+        return ans;
+    }
+};
+
+
+// Solve-2
 class Solution {
 public:
     int calculatePathSum(TreeNode* root, long long int targetSum) {
