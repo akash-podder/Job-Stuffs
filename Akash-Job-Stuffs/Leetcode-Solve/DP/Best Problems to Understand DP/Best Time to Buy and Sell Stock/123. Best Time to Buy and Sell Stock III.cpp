d2e80e020b6825b7244131ce9996e9e2415c093e
AkashPodder <anantaakash.podder@gmail.com>
2025-02-26 21:04:53 +0600

    - Tabulation Solve added for "123. Best Time to Buy and Sell Stock III"

--- Akash-Job-Stuffs/Leetcode-Solve/DP/Best Problems to Understand DP/Best Time to Buy and Sell Stock/123. Best Time to Buy and Sell Stock III.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/DP/Best Problems to Understand DP/Best Time to Buy and Sell Stock/123. Best Time to Buy and Sell Stock III.cpp
@@ -37,3 +37,71 @@ public:
         return solve(prices, 0, 1, 0, dp); // at first we want to Buy
     }
 };
+
+
+
+// ------------------------TABULATION SOLVE-----------------------
+
+class Solution {
+public:
+    int tabulation_solve(vector<int>& prices){
+        int n = prices.size();
+
+        // 3D Vector --> Because we kept (index, buy, txn_count) & we initialize with 0
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2,vector<int>(3, 0)));
+
+        // Base Case
+        // index==prices.size() then, return 0
+        for(int buy=0; buy<2; buy++){
+            for(int txn_count=0;txn_count<=2;txn_count++){
+                dp[n][buy][txn_count]=0;
+            }
+        }
+
+        // txn_count==2 then, return 0
+        for(int index=0;index<n;index++){
+            for(int buy=0; buy<2; buy++){
+                dp[index][buy][2]=0;
+            }
+        }
+
+        int ans=0;
+
+        for(int i=n-1;i>=0;i--){ // prices
+            for(int buy=0;buy<=1;buy++){ // Buy or Sell
+                for(int txn_count=0;txn_count<=1;txn_count++){ // Max-Count
+                    
+                    // We can BUY
+                    if(buy==true){
+                        dp[i][buy][txn_count] = max(
+                            // We decide to BUY
+                            -prices[i] + dp[i+1][0][txn_count],
+
+                            // We decide to SKIP BUY
+                            dp[i+1][1][txn_count]
+                        );
+                    }
+
+                    // We can SELL
+                    else{
+                        dp[i][buy][txn_count] = max(
+                            // we Decided to SELL, that's Why 1 TRANSACTION is Completed, so we INCREASE the "txn_count" with 1
+                            prices[i] + dp[i+1][1][txn_count+1],
+
+                            // we Decided to SKIP SELL
+                            dp[i+1][0][txn_count]
+                        );
+                    }
+                }
+            }
+        }
+
+        // as we called --> solve(prices, 0, true, 0, cache); --> so the value will be "dp[0][true][0]"
+        return dp[0][1][0];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        
+        return tabulation_solve(prices);
+    }
+};
