9ed76b6af291a2555ff07c11767df1ed1bf4ec20
AkashPodder <anantaakash.podder@gmail.com>
2025-04-25 14:06:37 +0600

    - comment documentation improved for `123. Best Time to Buy and Sell Stock III`

--- Akash-Job-Stuffs/Leetcode-Solve/DP/Best Problems to Understand DP/Best Time to Buy and Sell Stock/123. Best Time to Buy and Sell Stock III.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/DP/Best Problems to Understand DP/Best Time to Buy and Sell Stock/123. Best Time to Buy and Sell Stock III.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int index, bool buy, int transaction, vector<vector<vector<int>>> &dp) {
+    int solve(vector<int>& prices, int index, bool buy, int txn_count, vector<vector<vector<int>>> &dp) {
         // Base Case
-        if(index==prices.size() || transaction==2){
+        if(index==prices.size() || txn_count==2){
             return 0;
         }
 
-        if(dp[index][buy][transaction]!=-1){
-            return dp[index][buy][transaction];
+        if(dp[index][buy][txn_count]!=-1){
+            return dp[index][buy][txn_count];
         }
 
         int profit = 0;
 
-        // we CAN Buy
+        // buy==True --> we have OPTION to Buy
         if(buy==1){
             profit = max(
-                -prices[index] + solve(prices, index+1, 0, transaction, dp), // we Buy
-                0 + solve(prices, index+1, 1, transaction, dp) // we SKIP Buy
+                -prices[index] + solve(prices, index+1, 0, txn_count, dp), // we BOUGHT
+                0 + solve(prices, index+1, 1, txn_count, dp) // we Decided NOT to BUY
             );
         }
 
-        // we CAN Sell
+        // buy==False --> we have OPTION to Sell
         else{
             profit = max(
-                prices[index] + solve(prices, index+1, 1, transaction+1, dp), // we Sold --> 1 Transaction Complete
-                0 + solve(prices, index+1, 0, transaction, dp) // we SKIP Sell
+                prices[index] + solve(prices, index+1, 1, txn_count+1, dp), // we SOLD, as we Sold So we will Increase the "txn_count" to 1
+                0 + solve(prices, index+1, 0, txn_count, dp) // we Decided NOT to SELL
             );
         }
 
-        return dp[index][buy][transaction] = profit;
+        return dp[index][buy][txn_count] = profit;
     }
 
     int maxProfit(vector<int>& prices) {
@@ -50,22 +50,23 @@ public:
         // 3D Vector --> Because we kept (index, buy, txn_count) & we initialize with 0
         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2,vector<int>(3, 0)));
 
-        // Base Case
+        // Base Case START
         // index==prices.size() then, return 0
         for(int buy=0; buy<2; buy++){
             for(int txn_count=0;txn_count<=2;txn_count++){
                 dp[n][buy][txn_count]=0;
             }
         }
-
         // txn_count==2 then, return 0
         for(int index=0;index<n;index++){
             for(int buy=0; buy<2; buy++){
                 dp[index][buy][2]=0;
             }
         }
+        // Base Case END
 
-        int ans=0;
+        // buy= true --> 1
+        // buy= false --> 0
 
         for(int i=n-1;i>=0;i--){ // prices
             for(int buy=0;buy<=1;buy++){ // Buy or Sell
@@ -95,6 +96,9 @@ public:
                 }
             }
         }
+
+        // buy= true --> 1
+        // buy= false --> 0
 
         // as we called --> solve(prices, 0, true, 0, cache); --> so the value will be "dp[0][true][0]"
         return dp[0][1][0];
