4ba8c88704fa1930bdeab40aed871e95de3d5ed3
AkashPodder <anantaakash.podder@gmail.com>
2025-04-28 18:48:53 +0600

    - `91. Decode Ways` updated with Better Documentation in `Leetcode-Solve`

--- Akash-Job-Stuffs/Leetcode-Solve/DP/1D DP/91. Decode Ways.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/DP/1D DP/91. Decode Ways.cpp
@@ -1,48 +1,50 @@
 class Solution {
-    public:
-        int solve(string &s, int index, vector<int> &cache){
-            if(index==s.size()){
-                return 1; // as we are Counting Number of ways we just Return 1, this 1 will denote How Many Times we Hit the Bottom/End of the Recursive Tree. And while Calling the Recursive SOLVE Function we just “Add (+)” with it → ans = ans + solve(s, index+2, cache); → Like this
-            }
-    
-            if(cache[index]!=-1){
-                return cache[index];
-            }
-    
-            // if character is "0" We just Ommit it
-            if(s[index]=='0'){
-                return 0;
-            }
-            
+public:
+    int solve(string s, int index, vector<int> &cache){
+        // as we are Counting Number of ways we just Return 1, this 1 will denote How Many Times we Hit the Bottom/End of the Recursive Tree. And while Calling the Recursive SOLVE Function we just “Add (+)” with it → ans = ans + solve(s, index+2, cache); → Like this
+        if(index==s.size()){
+            return 1;
+        }
+
+        if(cache[index]!=-1){
+            return cache[index];
+        }
+        
+        int count=0;
+
+        // checking if the First Character is '0' or NOT, if '0' we Ommit it
+        if(s[index]>='1' && s[index]<='9'){
+
             // *** Think it like TAKE or NOT TAKE ***
             // But here is 2 TAKE Scenerios
 
             // At Every Index we can Decode in 2-ways...
             // 1st way ---> We Take the Current Char
             // 2nd way ---> We can ONLY TAKE if: COMBINE with the Next Character if these Two characters Form Less than 26
-            
-            int ans=0;
-    
+
+
             // 1st Way ---> Always TAKE
-            ans = solve(s, index+1, cache);
-    
+            count=solve(s, index+1, cache);
+            
+
             // 2nd Way... We can Only Take if between (10-26) Range
             // first we check if "(index+1)" Doesn't Exhaust the String
-            if(index+1<s.size()) {
+            if(index+1<s.size()){
                 // (10-26) --> for these Values
                 if(s[index]=='1' || (s[index]=='2' && s[index+1]>='0' && s[index+1]<='6')){
-                    ans = ans + solve(s, index+2, cache);
+                    count=count+solve(s, index+2, cache);
                 }
             }
-            
-    
-            return cache[index] = ans;
         }
+
     
-    
-        int numDecodings(string s) {
-            vector<int> cache(s.size(), -1);
-    
-            return solve(s, 0, cache);
-        }
-    };
+        return cache[index] = count;
+    }
+
+    int numDecodings(string s) {
+        int len = s.size();
+        vector<int> cache(len, -1);
+
+        return solve(s, 0, cache);
+    }
+};
