3805afb0dbfc6446e74942711de2c731c9193aca
AkashPodder <anantaakash.podder@gmail.com>
2025-04-29 15:24:04 +0600

    - `91. Decode Ways` tabulation solve added

--- Akash-Job-Stuffs/Leetcode-Solve/DP/1D DP/91. Decode Ways.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/DP/1D DP/91. Decode Ways.cpp
@@ -48,3 +48,58 @@ public:
         return solve(s, 0, cache);
     }
 };
+
+
+//-------------- TABULATION SOLVE ---------------
+class Solution {
+public:
+    int tabulation_solve(string s){
+        int len = s.size();
+        vector<int> dp(len+1, 0);
+        
+        // as we are Counting Number of ways we just Return 1, this 1 will denote How Many Times we Hit the Bottom/End of the Recursive Tree. And while Calling the Recursive SOLVE Function we just “Add (+)” with it → ans = ans + solve(s, index+2, cache); → Like this
+        
+        // Base Case: if(index==s.size()) {return 1;}
+        dp[len] = 1;
+        
+
+        for(int index=len-1; index>=0; index--){
+            int count=0;
+
+            // checking if the First Character is '0' or NOT, if '0' we Ommit it
+            if(s[index]>='1' && s[index]<='9'){
+
+                // *** Think it like TAKE or NOT TAKE ***
+                // But here is 2 TAKE Scenerios
+
+                // At Every Index we can Decode in 2-ways...
+                // 1st way ---> We Take the Current Char
+                // 2nd way ---> We can ONLY TAKE if: COMBINE with the Next Character if these Two characters Form Less than 26
+
+
+                // 1st Way ---> Always TAKE
+                count=dp[index+1];
+                
+
+                // 2nd Way... We can Only Take if between (10-26) Range
+                // first we check if "(index+1)" Doesn't Exhaust the String
+                if(index+1<s.size()){
+                    // (10-26) --> for these Values
+                    if(s[index]=='1' || (s[index]=='2' && s[index+1]>='0' && s[index+1]<='6')){
+                        count=count+dp[index+2];
+                    }
+                }
+            }
+
+
+            dp[index] = count;
+        }
+
+        // return solve(s, 0, cache);
+        return dp[0]; // as in "Recursive" Solution we are Starting from "0" index, so we will get our answer in dp[0]
+    }
+
+    int numDecodings(string s) {
+        return tabulation_solve(s);
+    }
+};
