f6330c300bd015e39973d449c6d155d08e5e32f5
AkashPodder <anantaakash.podder@gmail.com>
2025-05-17 18:50:21 +0600

    - `Bit Manipulation` comments added & also `260. Single Number III` solve-1 added

--- Akash-Job-Stuffs/Leetcode-Solve/Bit Manipulation/Single Number/260. Single Number III.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/Bit Manipulation/Single Number/260. Single Number III.cpp
@@ -1,3 +1,61 @@
+// ------------------------ SOLVE-1 ---------------------------------
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int twoNumberXor = 0;
+
+        for(int i=0; i<nums.size();i++){
+            twoNumberXor=twoNumberXor^nums[i];
+        }
+
+        vector<int> list1;
+        vector<int> list2;
+
+        // in XOR --> Two Digits SAME = 0, Two Digits DIFFERENT = 1
+        for(int k=0; k<=31; k++){
+            // "1" means the Digit Differ, Now we divide the List in TWO Parts based on that Digit's Position...
+
+            // Do Remember:
+            // Single “&” is for Bitwise “AND”
+
+            if((twoNumberXor & (1<<k))>0){
+                cout<<k<<endl;
+                for(int i=0; i<nums.size();i++){
+                    if((nums[i] & (1<<k)) > 0){
+                        list1.push_back(nums[i]);
+                    }
+                    else{
+                        list2.push_back(nums[i]);
+                    }
+                }
+
+                break;
+            }
+        }
+
+        int a=0, b=0;
+
+        for(int i=0; i<list1.size();i++){
+            a = a^list1[i];
+        }
+
+        for(int i=0; i<list2.size();i++){
+            cout<<list2[i]<<" ";
+            b = b^list2[i];
+        }
+        
+        vector<int> ans;
+
+        ans.push_back(a);
+        ans.push_back(b);
+
+        return ans;
+    }
+};
+
+
+
+// ------------------------ SOLVE-2 ---------------------------------
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
