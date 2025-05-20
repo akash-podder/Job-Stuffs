531a21061d5dac740c5cca5230239a328f185685
AkashPodder <anantaakash.podder@gmail.com>
2025-05-20 17:16:42 +0600

    - `189. Rotate Array` 's `reverseArray` better implemented

--- Akash-Job-Stuffs/Leetcode-Solve/Array & String/189. Rotate Array.cpp
+++ Akash-Job-Stuffs/Leetcode-Solve/Array & String/189. Rotate Array.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-void reverse_array(vector<int>& nums, int start, int end){
-    int len = end - start;
-    
-    for(int i=start; i<=start+(len/2); i++){
-        swap(nums[i], nums[start+end-i]);
-    }
-}
-
-void rotate(vector<int>& nums, int k) {
-    int len = nums.size();        
-    k = k%len;
-
-    // Edge Case: if k=0, then No Rotation Call is Needed
-    if(k==0){
-        return;
+    void reverseArray(vector<int>& nums, int start, int end){
+        while(start<=end){
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
     }
 
-    // 3 steps --> Reverse the 2 Halves, then Reverse the Whole Array
-    
-    // Reverse --> 0th index upto (len-k) index
-    reverse_array(nums, 0, len-k-1);
-    
-    // Reverse --> (len-k) index upto to Last index
-    reverse_array(nums, len-k, len-1);
+    void rotate(vector<int>& nums, int k) {
+        int len = nums.size();        
+        k = k%len;
 
-    // Reverse --> 0th to Last index
-    reverse_array(nums, 0, len-1);
-}
+        // Edge Case: if k=0, then No Rotation Call is Needed
+        if(k==0){
+            return;
+        }
+
+        // 3 steps --> Reverse the 2 Halves, then Reverse the Whole Array
+        
+        // Reverse --> 0th index upto (len-k) index
+        reverseArray(nums, 0, len-k-1);
+        
+        // Reverse --> (len-k) index upto to Last index
+        reverseArray(nums, len-k, len-1);
+
+        // Reverse --> 0th to Last index
+        reverseArray(nums, 0, len-1);
+    }
 };
